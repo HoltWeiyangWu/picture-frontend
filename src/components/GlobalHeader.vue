c468f3ebfcb41d24730ae6d031e9d6a8720f6095
HoltWeiyangWu <holtwyw@hotmail.com>
2025-04-01 10:36:31 +1100

    Add layout and global header 1. Set up layout with header, content and footer 2. Add global header 3. All built upon an added template

---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,90 @@
+<template>
+  <a-row :wrap="false">
+    <a-col flex="200px">
+      <RouterLink to="/">
+        <div class="title-bar">
+          <img class="logo" src="../assets/cloud.png" alt="logo">
+          <div class="title">Picture Cloud</div>
+        </div>
+      </RouterLink>
+    </a-col>
+    <a-col flex="auto">
+      <a-menu v-model:selectedKeys="current"
+              mode="horizontal"
+              :items="items"
+              @click="doMenuClick"/>
+    </a-col>
+    <a-col flex="120px">
+      <div class="user-login-status">
+        <a-button type="primary" href="/user/login">Login</a-button>
+      </div>
+    </a-col>
+  </a-row>
+
+</template>
+
+<script setup lang="ts">
+import {h, ref} from 'vue';
+import {HomeOutlined} from '@ant-design/icons-vue';
+import {MenuProps} from 'ant-design-vue';
+import {useRouter} from "vue-router";
+
+
+const items = ref<MenuProps['items']>([
+  {
+    key: '/',
+    icon: () => h(HomeOutlined),
+    label: 'Home',
+    title: 'Home',
+  },
+  {
+    key: '/about',
+    label: 'About',
+    title: 'About',
+  },
+
+  {
+    key: 'others',
+    label: h(
+        'a',
+        {
+          href: 'https://github.com/HoltWeiyangWu',
+          target: '_blank',
+        },
+        "Developer"
+    ),
+    title: 'Developer',
+  },
+]);
+
+// Add router
+const router = useRouter();
+// Highlight the selected item
+const current = ref<string[]>([]);
+router.afterEach((to, _, _)=> {
+  current.value = [to.path];
+})
+// Update route
+const doMenuClick = ({key}: { key: string }) => {
+  router.push({
+    path: key,
+  })
+}
+</script>
+
+<style scoped>
+.title-bar {
+  display: flex;
+  align-items: center;
+}
+
+.title {
+  color: black;
+  font-size: 18px;
+  margin-left: 16px;
+}
+
+.logo {
+  height: 48px;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
-import App from './App.vue'
 
-createApp(App).mount('#app')
+import router from './router'
+import { createPinia } from 'pinia'
+import Antd from 'ant-design-vue'
+import App from './App.vue'
+import 'ant-design-vue/dist/reset.css'
+
+const app = createApp(App)
+app.use(Antd)
+app.use(createPinia())
+app.use(router)
+app.mount('#app')
