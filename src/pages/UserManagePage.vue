1472640ac7334f44f5b3109286af80af87a45a36
HoltWeiyangWu <holtwyw@hotmail.com>
2025-04-09 13:07:22 +1000

    Add comments and fix bug 1. Fix minor bug and add comments

--- src/pages/UserManagePage.vue
+++ src/pages/UserManagePage.vue
@@ -4,8 +4,8 @@ import {computed, onMounted, reactive, ref} from "vue";
 import {deleteUser, listUserVoByPage, updateUser} from "@/api/userController.ts";
 import {DeleteOutlined, EditOutlined, CheckOutlined, CloseOutlined} from "@ant-design/icons-vue"
 import {message} from "ant-design-vue";
-import { cloneDeep } from 'lodash-es';
-import type { UnwrapRef } from 'vue';
+import {cloneDeep} from 'lodash-es';
+import type {UnwrapRef} from 'vue';
 import dayjs from "dayjs";
 import {useLoginUserStore} from "@/stores/user.ts";
 
@@ -66,11 +66,11 @@ const fetchData = async () => {
 }
 
 // Load data for the first request
-onMounted(()=>{
+onMounted(() => {
   fetchData()
 })
 
-const pagination = computed(()=>{
+const pagination = computed(() => {
   return {
     current: searchParams.current ?? 1,
     pageSize: searchParams.pageSize ?? 10,
@@ -135,6 +135,7 @@ const doCancel = (key: string) => {
 
 </script>
 <template>
+  <!--   Search bar to filter user list-->
   <a-form layout="inline" :model="searchParams" @finish="doSearch">
     <a-form-item label="Account">
       <a-input v-model:value="searchParams.userAccount" placeholder="Search account"></a-input>
@@ -147,50 +148,51 @@ const doCancel = (key: string) => {
     </a-form-item>
   </a-form>
 
-  <a-table :columns="columns" :data-source="dataList" :pagination="pagination" @change="doTableChange" table-layout="fixed">
+  <a-table :columns="columns" :data-source="dataList" :pagination="pagination" @change="doTableChange"
+           table-layout="fixed">
     <template #headerCell="{ column }">
     </template>
 
     <template #bodyCell="{column, record}">
-
+      <!--Display or update fields-->
       <template v-if="['userName', 'userAvatar', 'userProfile', 'userRole'].includes(column.dataIndex)">
-        <div>
-          <a-input v-if="editableData[record.id] && column.dataIndex != 'userRole'"
+        <a-input v-if="editableData[record.id] && column.dataIndex != 'userRole'"
+                 v-model:value="editableData[record.id][column.dataIndex]"
+                 style="margin: -5px 0"/>
+        <a-radio-group
+            v-else-if="editableData[record.id] && column.dataIndex == 'userRole'"
             v-model:value="editableData[record.id][column.dataIndex]"
-          style="margin: -5px 0"/>
-          <a-radio-group
-              v-else-if="editableData[record.id] && column.dataIndex == 'userRole'"
-              v-model:value="editableData[record.id][column.dataIndex]"
-              button-style="solid">
-              <a-radio-button value="admin">Admin</a-radio-button>
-              <a-radio-button value="user">User</a-radio-button>
-          </a-radio-group>
-          <template v-else>
-            <template v-if="column.dataIndex === 'userAvatar'">
-              <a-image :src="record.userAvatar" :width="120"/>
-            </template>
-            <template v-if="column.dataIndex === 'userName'">
-              {{ record.userName }}
-            </template>
-            <template v-if="column.dataIndex === 'userProfile'">
-              {{ record.userProfile }}
-            </template>
-            <template v-else-if="column.dataIndex === 'userRole'">
-              <div v-if="record.userRole === 'admin'">
-                <a-tag color="green">Admin</a-tag>
-              </div>
-              <div v-else>
-                <a-tag color="blue">User</a-tag>
-              </div>
-            </template>
+            button-style="solid">
+          <a-radio-button value="admin">Admin</a-radio-button>
+          <a-radio-button value="user">User</a-radio-button>
+        </a-radio-group>
+        <template v-else>
+          <template v-if="column.dataIndex === 'userAvatar'">
+            <a-image :src="record.userAvatar"/>
           </template>
-        </div>
+          <template v-if="column.dataIndex === 'userName'">
+            {{ record.userName }}
+          </template>
+          <template v-if="column.dataIndex === 'userProfile'">
+            {{ record.userProfile }}
+          </template>
+          <template v-else-if="column.dataIndex === 'userRole'">
+            <div v-if="record.userRole === 'admin'">
+              <a-tag color="green">Admin</a-tag>
+            </div>
+            <div v-else>
+              <a-tag color="blue">User</a-tag>
+            </div>
+          </template>
+        </template>
       </template>
 
-
+      <!--Fixed fields-->
       <template v-else-if="column.dataIndex === 'createTime'">
-        {{dayjs(record.createTime).format('YYYY-MM-DD HH:mm:ss')}}
+        {{ dayjs(record.createTime).format('YYYY-MM-DD HH:mm:ss') }}
       </template>
+
+      <!--Operations for admins-->
       <template v-else-if="column.key === 'action'">
         <div>
           <span v-if="editableData[record.id]">
@@ -199,14 +201,15 @@ const doCancel = (key: string) => {
           </span>
           <span v-else>
             <EditOutlined @click="doEdit(record.id)" style="margin-right: 20px"/>
-          <a-popconfirm title="Confirm to delete this user?" @confirm="doDelete(record.id)" placement="topRight">
-            <DeleteOutlined v-if="record.id != loginUserStore.loginUser?.id" style="color: red;" />
+          <a-popconfirm title="Confirm to delete this user?"
+                        @confirm="doDelete(record.id)"
+                        placement="topRight">
+            <DeleteOutlined v-if="record.id != loginUserStore.loginUser?.id"
+                            style="color: red;"/>
           </a-popconfirm>
           </span>
         </div>
       </template>
-
-
     </template>
   </a-table>
 </template>
